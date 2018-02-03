99a685530848b8e3b61ccfa91fe601c743578295
Thai Tao <thaitao2702@gmail.com>
2018-02-03 18:21:39 +0700

    optimize code

--- codecamp3/Program.cs
+++ codecamp3/Program.cs
@@ -8,28 +8,22 @@ namespace codecamp3
     {
         static void Main(string[] args)
         {
-            string[] w ={"she","ite","we","you","thy","h"};
-            GetTheLastWord(w);
+            bai1();
         }
         
         static void bai1(){
             string[] cities =  {  
                 "ROME","ZURICH","AMSTERDAM","SAIGON", "LONDON","HANOI","CALIFORNIA", "PARIS"  
             }; 
-            var filter1 = from x in cities orderby x.Length ascending select x;
-            var filter2 = filter1.GroupBy(x => x.Length);
-            foreach (var group in filter2){
-              var filter3 = group.OrderBy(x=>x);
-              foreach(var item in filter3){
-                Console.WriteLine(item);
-              }
+            var result = cities.OrderBy(x=> x.Length).GroupBy(x=>x.Length).Select(group=>group.OrderBy(x=>x)).SelectMany(x=>x);
+            foreach (var item in result){
+                System.Console.WriteLine(item);
             }
         }
         public static bool TestForSquares(int[] numbers, int[] squares) {
             var sq_numbers = from item in numbers let sq = item*item select sq;
             var result = squares.Intersect(sq_numbers);
-            if (result.Count()==squares.Length && result.Count()==numbers.Length){
-                System.Console.WriteLine("true");
+            if (result.Count()==squares.Length && result.Count()==numbers.Length){                
                 return true;
             }
             else return false;
